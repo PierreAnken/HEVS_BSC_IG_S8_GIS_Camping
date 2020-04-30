cbec75a3e75ce58c8af522518e5113b5154f11be
Pierre Anken <pierre.anken@gmail.com>
2020-04-30 12:39:58 +0200

    PSQL config added

--- HEVS_S8_GIS_Camping/HEVS_S8_GIS_Camping/settings.py
+++ HEVS_S8_GIS_Camping/HEVS_S8_GIS_Camping/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'HEVS_S8_GIS_Camping.wsgi.application'
 # TODO: everyone creates local database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': "django.contrib.gis.db.backends.postgis",
+        'NAME': 'campMap',
+        'USER': 'admin',
+        'PASSWORD': 'adminPWD',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
 
