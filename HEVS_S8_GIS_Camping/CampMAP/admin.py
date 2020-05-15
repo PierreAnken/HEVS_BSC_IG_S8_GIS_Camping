a897d257c677e8846c6c1165866144e0dc572422
Buubu <montaine@windowslive.com>
2020-05-15 11:57:23 +0200

    Update admin.py

--- HEVS_S8_GIS_Camping/CampMAP/admin.py
+++ HEVS_S8_GIS_Camping/CampMAP/admin.py
@@ -1,19 +1,10 @@
 from django.contrib.gis import admin
 from .models import *
 
+
+# *** Models registered ***
 admin.site.register(Place, admin.OSMGeoAdmin)
 admin.site.register(Building, admin.OSMGeoAdmin)
 admin.site.register(CampingArea, admin.OSMGeoAdmin)
 admin.site.register(Pool, admin.OSMGeoAdmin)
 admin.site.register(Tree, admin.OSMGeoAdmin)
-
-
-# *** EXAMPLES ***
-# from .models import City, Hospital, Canton
-
-
-# Register your models here.
-
-# *** EXAMPLES ***
-# admin.site.register(City)
-# admin.site.register(Hospital)
