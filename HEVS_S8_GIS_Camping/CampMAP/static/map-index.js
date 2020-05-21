fd59b965eb6ce797653a903e3b059c0dd4220ef6
Buubu <montaine@windowslive.com>
2020-05-21 17:06:29 +0200

    Define neutral index page
    TODO: register and login actions

---
+++ HEVS_S8_GIS_Camping/CampMAP/static/map-index.js
@@ -0,0 +1,64 @@
+function initialize() {
+
+    // **** Define the base tile layer ****
+    var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+
+    // **** Define the overlay ****
+    var camping_areaslayer = L.layerGroup();
+
+
+    // **** Create the leaflet map ****
+    var map = L.map('campingmap', { minZoom: 10, maxZoom: 18 }).setView([46.211606, 7.3167], 18);
+
+    var campingareasfile = '/campingareas.json';
+    $.getJSON(campingareasfile, function (data) {
+        campingareas = L.geoJson(data, { onEachFeature: onEachFeature });
+        campingareas.addTo(camping_areaslayer);
+    });
+
+
+    // **** Assemble layers ****
+    OpenStreetMap_Mapnik.addTo(map);
+    camping_areaslayer.addTo(map);
+
+
+    // **** Decorate feature ****
+    function onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: highlight,
+            mouseout: reset,
+            click: alertUser
+        });
+    }
+
+
+    // **** Define the highlight functions ****
+    function highlight(e) {
+        console.log(e.target);
+        var layer = e.target;
+        layer.setStyle({ weight: 5, color: "#ffeb01", backgroundColor: "#ffeb01", dashArray: "", fillOpacity: 0.7 });
+    }
+
+    function reset(e) {
+        campingareas.resetStyle(e.target);
+    }
+
+
+    // **** Alert anonymous user with popup ****
+    var popup = L.popup();
+
+    function alertUser(e) {
+        popup.setLatLng(e.latlng)
+            .setContent('<div class="modal-body">' +
+                '<text class="popup-index-title">You are not authorized to access</text><br/><br/>' +
+                '<text class="popup-index-text">Please login with authenticated account to perform some action.<br/>' +
+                'Or register your account by clicking the button below.</text>' +
+                '<button type="button" class="btn-sm btn-popup-index">Register ></button>' +
+                '</div>')
+            .openOn(map);
+    }
+}
