6cb4e5273d026ff663af3f2394b0b1cd134a330e
Buubu <montaine@windowslive.com>
2020-05-22 19:31:09 +0200

    Remove Users/Groups from admin page

--- HEVS_S8_GIS_Camping/CampMAP/admin.py
+++ HEVS_S8_GIS_Camping/CampMAP/admin.py
@@ -1,8 +1,11 @@
 from django.contrib.gis import admin
+from django.contrib.auth.models import Group
 from .models import *
 
 
 # *** Models registered ***
+admin.site.unregister(User)
+admin.site.unregister(Group)
 admin.site.register(Place, admin.OSMGeoAdmin)
 admin.site.register(Building, admin.OSMGeoAdmin)
 admin.site.register(CampingArea, admin.OSMGeoAdmin)
