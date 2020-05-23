eb84ccd14a68f93893b7eee1d119dbdf0ff1d55e
Buubu <montaine@windowslive.com>
2020-05-23 18:22:16 +0200

    Add register form to create an account

---
+++ HEVS_S8_GIS_Camping/CampMAP/classes/RegisterForm.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=32)
+    last_name = forms.CharField(max_length=32)
+    email = forms.EmailField(max_length=64, help_text="Enter a valid email address")
+    adults = forms.IntegerField(help_text="Indicate the number of adults")
+    kids = forms.IntegerField(help_text="Indicate the number of children")
+    pets = forms.BooleanField()
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email', 'adults', 'kids', 'pets')
