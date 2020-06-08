f702417654d70f33a83c20fb6f6f570f94e1abd6
mia <genttunn@gmail.com>
2020-06-08 21:17:12 +0300

    added icons to login, change neighbor slider min from 0 to 1

--- HEVS_S8_GIS_Camping/CampMAP/static/map-index.js
+++ HEVS_S8_GIS_Camping/CampMAP/static/map-index.js
@@ -1,3 +1,22 @@
+let poolIcon = L.icon({
+    iconUrl: 'https://image.flaticon.com/icons/svg/2972/2972208.svg',
+
+    iconSize: [23, 55], // size of the icon
+    shadowSize: [30, 50], // size of the shadow
+    iconAnchor: [12, 35], // point of the icon which will correspond to marker's location
+    shadowAnchor: [-6, 70],  // the same for the shadow
+    popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
+});
+let buildingIcon = L.icon({
+    iconUrl: 'https://image.flaticon.com/icons/svg/869/869636.svg',
+
+    iconSize: [23, 55], // size of the icon
+    shadowSize: [30, 50], // size of the shadow
+    iconAnchor: [12, 35], // point of the icon which will correspond to marker's location
+    shadowAnchor: [-6, 70],  // the same for the shadow
+    popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
+});
+
 function initialize() {
 
     // **** Define the base tile layer ****
@@ -12,22 +31,22 @@ function initialize() {
     var pools_layer = L.layerGroup();
 
     // **** Create the leaflet map ****
-    var map = L.map('campingmap', { minZoom: 10, maxZoom: 20 }).setView([46.211606, 7.3167], 18);
+    var map = L.map('campingmap', {minZoom: 10, maxZoom: 20}).setView([46.211606, 7.3167], 18);
 
     var campingareasfile = '/campingareas.json/';
     $.getJSON(campingareasfile, function (data) {
-        campingareas = L.geoJson(data, { onEachFeature: onEachFeature });
+        campingareas = L.geoJson(data, {onEachFeature: decorateEachFeature("campingarea")});
         campingareas.addTo(camping_areaslayer);
     });
     var buildingsfile = '/buildings.json/';
     $.getJSON(buildingsfile, function (data) {
-        buildings = L.geoJson(data, { onEachFeature: onEachFeature });
+        buildings = L.geoJson(data, {onEachFeature: decorateEachFeature('building')});
         buildings.addTo(buildings_layer);
     });
 
     var poolsfile = '/pools.json/';
     $.getJSON(poolsfile, function (data) {
-        pools = L.geoJson(data, { onEachFeature: onEachFeature });
+        pools = L.geoJson(data, {onEachFeature: decorateEachFeature('pool')});
         pools.addTo(pools_layer);
     });
     // **** Assemble layers ****
@@ -35,7 +54,39 @@ function initialize() {
     camping_areaslayer.addTo(map);
     buildings_layer.addTo(map);
     pools_layer.addTo(map);
+
     // **** Decorate feature ****
+    function decorateEachFeature(featureType) {
+        switch (featureType) {
+            case "campingarea":
+                return function onEachPool(feature, layer) {
+                    layer.on({
+                        mouseover: highlight,
+                        mouseout: reset,
+                        click: alertUser
+                    });
+                }
+            case "pool":
+                return function onEachPool(feature, layer) {
+                    layer.on({
+                        mouseover: highlight,
+                        mouseout: reset,
+                        click: alertUser
+                    });
+                    L.marker(layer.getBounds().getCenter(), {icon: poolIcon}).addTo(pools_layer);
+                }
+            case "building":
+                return function onEachBuilding(feature, layer) {
+                    layer.on({
+                        mouseover: highlight,
+                        mouseout: reset,
+                        click: alertUser
+                    });
+                    L.marker(layer.getBounds().getCenter(), {icon: buildingIcon}).addTo(buildings_layer);
+                }
+        }
+    }
+
     function onEachFeature(feature, layer) {
         layer.on({
             mouseover: highlight,
@@ -47,9 +98,8 @@ function initialize() {
 
     // **** Define the highlight functions ****
     function highlight(e) {
-        console.log(e.target);
         var layer = e.target;
-        layer.setStyle({ weight: 5, color: "#ffeb01", backgroundColor: "#ffeb01", dashArray: "", fillOpacity: 0.7 });
+        layer.setStyle({weight: 5, color: "#ffeb01", backgroundColor: "#ffeb01", dashArray: "", fillOpacity: 0.7});
     }
 
     function reset(e) {
